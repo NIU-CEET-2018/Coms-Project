6fd9ad48c33bb88127789ba37fc9e2b7102b6cef
Murray Fordyce <Murray.Fordyce@gmail.com>
2018-05-20 23:59:06 -0500

    use a better voice synth when there is wifi

--- SpeechSynth.py
+++ SpeechSynth.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """Converts text to speach."""
 
+from google_speech import Speech
 import pyttsx3
 engine = pyttsx3.init()
 
 
 def sayText(t):
-    engine.say(t)
-    engine.runAndWait()
+    def sayWithGoogle():
+        Speech(t, "en").play(("Speed", "1.2"))
+
+    def sayWithESpeak():
+        engine.say(t)
+        engine.runAndWait()
+
+    try:
+        sayWithGoogle()
+    except:
+        sayWithESpeak()
