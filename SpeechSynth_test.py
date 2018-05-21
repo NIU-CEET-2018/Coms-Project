29b806ffe62c46658fc1b4f9898a860ca50fbe60
Murray Fordyce <Murray.Fordyce@gmail.com>
2018-05-20 23:58:15 -0500

    made the first test

--- SpeechSynth_test.py
+++ SpeechSynth_test.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import unittest
+import SpeechSynth
 
 
 class SpeechSynthTests(unittest.TestCase):
         def setUp(self):
                 pass
+
+        def test_can_say_words(self):
+                SpeechSynth.sayText("This is a test.")
 
         def tearDown(self):
                 pass
