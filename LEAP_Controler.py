e34fbd4f02858a273f88e7a8dc2f7e324bf30da4
Murray Fordyce <unDeadHerbs@gmail.com>
2018-05-26 16:28:32 -0500

    changed features to those that we need (two hands and position)

--- LEAP_Controler.py
+++ LEAP_Controler.py
@@ -20,35 +20,44 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 controller = Leap.Controller()
 
+def valid_hands(h):
+    if len(h)==1:
+        return True
+    if len(h)==2:
+        if h[0].is_right():
+            return h[1].is_left()
+        return h[1].is_right()
+    return False
+
 def get_hand_position(blocking=False):
     # TODO: convert to a timeout
     frame = controller.frame()
-    if not blocking and len(frame.fingers) == 0:
+    if not blocking and not valid_hands(frame.hands):
         return None
 
-    while len(frame.fingers) == 0:
+    while not valid_hands(frame.hands):
         frame = controller.frame()
 
-    # TODO: two hands will usualy be present
-    fingers = controller.frame().fingers
-    finger_bones = []
-    for finger in fingers:
-        finger_bones.append(finger.bone(Bone.TYPE_METACARPAL).next_joint)
-        finger_bones.append(finger.bone(Bone.TYPE_PROXIMAL).next_joint)
-        finger_bones.append(finger.bone(Bone.TYPE_INTERMEDIATE).next_joint)
-        finger_bones.append(finger.bone(Bone.TYPE_DISTAL).next_joint)
+    calibrated_finger_bones = collections.OrderedDict()
 
-    # possible issue when more than one hand
-    hands = controller.frame().hands
-    hand_center = 0
-    for hand in hands:
+    for hand in frame.hands:
         hand_center = hand.palm_position
+        finger_bones = []
+        for finger in hand.fingers:
+            finger_bones.append(finger.bone(Bone.TYPE_METACARPAL).next_joint)
+            finger_bones.append(finger.bone(Bone.TYPE_PROXIMAL).next_joint)
+            finger_bones.append(finger.bone(Bone.TYPE_INTERMEDIATE).next_joint)
+            finger_bones.append(finger.bone(Bone.TYPE_DISTAL).next_joint)
 
-        calibrated_finger_bones = collections.OrderedDict()
+        # always 20 = 5 fingers * 4 joionts
         for i in range(len(finger_bones)):
             normalized_joint = (finger_bones[i] - hand_center).to_tuple()
             for j in range(3):
-                calibrated_finger_bones["feat" + str(i*3+j)] = normalized_joint[j]
+                # if only left or right hand some features will be missing
+                # TODO: set them to 0
+                calibrated_finger_bones["feat" + str(hand.is_right()*(5*4*3+3)+i*3+j+3)] = normalized_joint[j]
+        for j in range(3):
+            calibrated_finger_bones["feat"+str(hand.is_right()*(5*4*3+3)+j)]=hand_center[j]
 
     # TODO: here goes the physics filter
 
