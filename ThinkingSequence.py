e7ebec77ae628a555e533967ef7467134abc3178
Murray Fordyce <Murray.Fordyce@gmail.com>
2018-06-08 16:13:24 -0500

    started work on the main object between the AIs

---
+++ ThinkingSequence.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""A class that represents an updating vector.
+
+The class described here in is similar to a const vector reference in that the items within is could be manipulated still by the providing thread, but the receiving thread is unable to change the values if finds (only read them). This class will also have the ability to inform others when changes have occurred and possibility the ability to hook functions into it from the client side so that they can be automatically called upon an update occurring (possibly non-threaded)."""
+
+class ThinkingSequence():
+    def __init__():
+        pass
---
+++ ThinkingSequence_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import unittest
+from ThinkingSequence import ThinkingSequence
+
+
+class ThinkingSequenceeTests(unittest.TestCase):
+        def setUp(self):
+                pass
+
+        def tearDown(self):
+                pass
