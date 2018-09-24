47084626f83e5aab90eb6f4b5348c255774e8f02
Marianne Guieb <mguieb2@gmail.com>
2018-09-24 00:14:53 -0500

    last commit for the night

--- Physics_Filter_demo.py
+++ Physics_Filter_demo.py
@@ -5,43 +5,72 @@
 import numpy
 import Physics_Filter
 import matplotlib.pyplot as plt
+import random
+
+# DESCRIPTION:
+# This code was written to demonstrate Physics_Filter
+# Dummy data was created to be fed into the filter
+# Kalman Filter is modelled a little differently for this, but general logic remains the same
 
 # TODO:
 # - [ ] Run the code and verify
 # - [ ] Make the plots look prettier
+#       - Create titles
+#       - Create axis titles
+#       - Create legends
+#       - Make it colorful
+
+# NOTE:
+# The success of this relies heavily on how well we model the system and how well we quantify the covariances
+# The validity of this demo doesn't guarantee us a good Kalman Filter for American Sign Language
+# The ASL Kalman Filter is modelled differently than the demo Kalman Filter
 
 # GLOBAL VARIABLES
 initialPosition = (0, 0, 0)
 initialVelocity = (5, 100, 10)
-Matrix_size = 100
+MatrixSize = 100
 whiteNoiseSTDev = 9
 deltaT = 0.5
 
-stateTransition = numpy.matrix('(1, 1, 1), (deltaT, deltaT, deltaT), (0, 0.5*deltaT^2, 0); (0, 0, 0), (1, deltaT, 1), (0, 1, 0); (0, 0, 0), (0, 0, 0), (0, 1, 0)')
+stateTransition = numpy.matrix('(1, 1, 1), (deltaT, deltaT, deltaT), (0, 0.5*deltaT^2, 0); (0, 0, 0), (1, 1, 1), (0, deltaT, 0); (0, 0, 0), (0, 0, 0), (0, 1, 0)')
 
-    # HORIZONTAL MOTION: (X)
-    # acceleration = 0
-    # velocity = initialVelocity_x = constant
-    # position = initialPosition_x + initialVelocity_x*deltaT 
+# DERIVATION of stateTransition:
+
+# HORIZONTAL MOTION: (X)
+# position     = initialPosition_x + initialVelocity_x*deltaT 
+# velocity     = initialVelocity_x = constant
+# acceleration = 0
     
-    # VERTICAL MOTION: (y)
-    # acceleration = gravity = -9.98 m/s = constant
-    # velocity = initialVelocity_y + acceleration*deltaT
-    # position = initialPosition_y + initialVelocity_y*deltaT + 0.5*acceleration*deltaT^2
+# VERTICAL MOTION: (y)
+# position     = initialPosition_y + initialVelocity_y*deltaT + 0.5*acceleration*deltaT^2
+# velocity     = initialVelocity_y + acceleration*deltaT
+# acceleration = gravity = -9.98 m/s = constant
     
-    # Z MOTION: 
-    # acceleration = 0
-    # velocity = initialVelocity_z = constant
-    # position = initialPosition_z + initialVelocity_z*deltaT
+# Z MOTION: 
+# position     = initialPosition_z + initialVelocity_z*deltaT
+# velocity     = initialVelocity_z = constant
+# acceleration = 0
     
-    # STATE TRANSITION MATRIX:
-    # stateVectors   stateTransition
-    # position     : (1, 1, 1), (deltaT, deltaT, deltaT), (0, 0.5*deltaT^2, 0)
-    # velocity     : (0, 0, 0), (1, deltaT, 1),           (0, 1, 0)
-    # acceleration : (0, 0, 0), (0, 0, 0),                (0, 1, 0)
+# STATE TRANSITION MATRIX:
+# tuple form where tuples are organized in (x, y, z) components
+
+# stateVectors   stateTransition
+# position     : (initPos_x, initPos_y, initPos_z), (delT*initVel_x, delT*initVel_y, delT*initVel_z), (0, 0.5*delT^2*accel, 0)
+# velocity     : (0,         0,         0        ), (initVel_x,      initVel_y,      initVel_z     ), (0, delT,             0)
+# acceleration : (0,         0,         0        ), (0,              0,              0             ), (0, accel,            0)
+
+# In other words, 
+# stateVectors   stateTransition
+# position     : (1, 1, 1), (deltaT, deltaT, deltaT), (0, 0.5*deltaT^2, 0)
+# velocity     : (0, 0, 0), (1,      1,      1     ), (0, deltaT,       0)
+# acceleration : (0, 0, 0), (0,      0,      0     ), (0, 1,            0)
     
-    # NOTE:
-    # The tuples are organized in (x, y, z) components
+# The tuples are organized in (x, y, z) components
+
+# Another translation of the maths above
+# constant velocity in the x direction with zero acceleration
+# constant velocity in the z direction with zero acceleration
+# constant acceleration in the y direction, simulating object falling
 
 def main():
     
@@ -54,39 +83,39 @@ def main():
     # "stateMatrix" - dummy sample data of object doing the thing it does
     randPosition = (random.randint(0,10), random.randint(0,10), random.randint(0,10))
     randVelocity = (random.randint(0,100), random.randin(50,100), random.randint(0,100))
-    randProcess = dataGenerator(randPosition, randVelocity, deltaT, MatrixSize, whiteNoiseSTDev, stateTransition)
+    randProcess  = dataGenerator(randPosition, randVelocity, deltaT, MatrixSize, whiteNoiseSTDev, stateTransition)
     
     # process the data a bit
-    staticPositionData = dataProcessor(0, whiteNoise)
-    staticVelocityData = dataProcessor(1, whiteNoise)
+    staticPositionData     = dataProcessor(0, whiteNoise)
+    staticVelocityData     = dataProcessor(1, whiteNoise)
     staticAccelerationData = dataProcessor(2, whiteNoise)
     
-    movingPositionData = dataProcessor(0, randProcess)
-    movingVelocityData = dataProcessor(1, randProcess)
+    movingPositionData     = dataProcessor(0, randProcess)
+    movingVelocityData     = dataProcessor(1, randProcess)
     movingAccelerationData = dataProcessor(2, randProcess)
     
     # obtain measurementNoise, stateMatrix, processNoise
-    Physics_Filter.setupKalmanFilterDEMO(staticPositionData, staticVelocityData, staticAccelerationData, movingPositionData, movingVelocity, movingAccelerationData)
+    Physics_Filter.setupKalmanFilterDEMO(staticPositionData, staticVelocityData, staticAccelerationData, movingPositionData, movingVelocityData, movingAccelerationData)
     
     for state in measuredStates:
-        filteredData = KalmanFilterxvaDEMO(measuredState, deltaT)
+        filteredData = Physics_Filter.KalmanFilterxvaDEMO(measuredStates, deltaT)
         
-    cleanData = canonSimulator(intialPosition, initialVelocity, deltaT, MatrixSize, stateTransition)
+    cleanData = canonSimulator(initialPosition, initialVelocity, deltaT, MatrixSize, stateTransition)
     
     # real-ish
     realData = measuredStates
     
     # process data a little to plot
-    realPosition = dataProcessor(0, realData)
-    realVelocity = dataProcessor(1, realData)
+    realPosition     = dataProcessor(0, realData)
+    realVelocity     = dataProcessor(1, realData)
     realAcceleration = dataProcessor(2, realData)
     
-    filteredPosition = dataProcessor(0, filteredData)
-    filteredVelocity = dataProcessor(1, filteredData)
+    filteredPosition     = dataProcessor(0, filteredData)
+    filteredVelocity     = dataProcessor(1, filteredData)
     filteredAcceleration = dataProcessor(2, filteredData)
     
-    cleanPosition = dataProcessor(0, cleanData)
-    cleanVelocity = dataProcessor(1, cleanData)
+    cleanPosition     = dataProcessor(0, cleanData)
+    cleanVelocity     = dataProcessor(1, cleanData)
     cleanAcceleration = dataProcessor(2, cleanData)
     
     # process the data a bit more 
@@ -102,6 +131,7 @@ def main():
     realAccelerationy = dataProcessor(1, realAcceleration)
     realAccelerationz = dataProcessor(2, realAcceleration)
     
+    # keep processing that data
     filteredPositionx = dataProcessor(0, filteredPosition)
     filteredPositiony = dataProcessor(1, filteredPosition)
     filteredPositionz = dataProcessor(2, filteredPosition)
@@ -114,6 +144,7 @@ def main():
     filteredAccelerationy = dataProcessor(1, filteredAcceleration)
     filteredAccelerationz = dataProcessor(2, filteredAcceleration)
     
+    # almost done processing data
     cleanPositionx = dataProcessor(0, cleanPosition)
     cleanPositiony = dataProcessor(1, cleanPosition)
     cleanPositionz = dataProcessor(2, cleanPosition)
@@ -129,6 +160,7 @@ def main():
     T = numpy.arange(0, MatrixSize*deltaT, deltaT)
     
     # plot realData, filteredData, and cleanData
+    # Position graphs
     plt.figure(1)
     plt.subplot(311)
     plt.plot(realPositionx, T)
@@ -143,6 +175,7 @@ def main():
     plt.plot(filteredPositionz, T)
     plt.plot(cleanPositionz, T)
     
+    # Velocity graphs
     plt.figure(2)
     plt.subplot(311)
     plt.plot(realVelocityx, T)
@@ -157,6 +190,7 @@ def main():
     plt.plot(filteredVelocityz, T)
     plt.plot(cleanVelocityz, T)
     
+    # Acceleration graphs
     plt.figure(3)
     plt.subplot(311)
     plt.plot(realAccelerationx, T)
@@ -165,7 +199,7 @@ def main():
     plt.subplot(312)
     plt.plot(realAccelerationy, T)
     plt.plot(filteredAccelerationy, T)
-    plt.plot(cleanAccelerationy, T)
+    plt.plot(cleanAccelerationz, T)
     plt.subplot(313)
     plt.plot(realAccelerationz, T)
     plt.plot(filteredAccelerationz, T)
@@ -175,7 +209,7 @@ def main():
     
 def dataProcessor(spot, array):
     
-    data = [thing[spot] in thing in array]
+    data = [x[spot] for x in array]
     
     return data
                     
@@ -187,24 +221,27 @@ def whiteNoiseGenerator(std, matrixSize):
     
 def canonSimulator(initialPos, initialVel, deltaT, matrixSize, stateTransition):
     
-    measuredState = numpy.zeros(matrixSize)
-    gravity = -9.98
+    measuredState       = numpy.zeros(matrixSize)
+    gravity             = -9.98
     initialAcceleration = (0, gravity, 0)
-    initialState = (initialPos, initialVel, initialAccel)
-    measuredState[0] = initialState
+    initialState        = (initialPos, initialVel, initialAcceleration)
+    measuredState[0]    = initialState
     
     index = 0
     
     while index < matrixSize: 
-        measuredState[states+1] = stateTransition*measuredState[states] 
+        measuredState[index+1] = stateTransition*measuredState[index] 
         index += index
         
     return measuredState
 
 def dataGenerator(initialPos, initialVel, deltaT, matrixSize, whiteNoiseSTD, stateTransition):
     
-    whiteNoise = whiteNoiseGenerator(whiteNoiseSTD, matrixSize)
-    canonSimulation = canonSimulator(initialPos, initialVel, deltaT, matrixSize, stateTransition)
+    whiteNoise           = whiteNoiseGenerator(whiteNoiseSTD, matrixSize)
+    canonSimulation      = canonSimulator(initialPos, initialVel, deltaT, matrixSize, stateTransition)
     noisyCanonSimulation = canonSimulation + whiteNoise
     
     return noisyCanonSimulation
+
+if __name__ == "__main__":
+    main()
