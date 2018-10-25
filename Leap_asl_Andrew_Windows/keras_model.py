d4d81e9d535b4d158b328fb5e15843f71855eea9
Murray Fordyce <Murray.Fordyce@gmail.com>
2018-10-25 18:50:37 -0500

    Many much lint begon, also a better loop

--- Leap_asl_Andrew_Windows/keras_model.py
+++ Leap_asl_Andrew_Windows/keras_model.py
@@ -1,6 +1,5 @@
-import numpy as np
-import pandas as pd
 import os
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -8,33 +7,31 @@ from keras.layers import Dropout
 
 from sklearn.model_selection import train_test_split
 
-
-
 #formats data to all the same size and removes timestamp
 def reshape(y):
-    y = np.delete(y, 0 ,0)
-    z = np.delete(y,-1,1)
+    y = np.delete(y, 0, 0)
+    z = np.delete(y, -1, 1)
     numrows = len(z)
-    if numrows <=50:
-        shape=(50,37)
+    if numrows <= 50:
+        shape = (50, 37)
         result = np.zeros(shape)
-        result[:z.shape[0],:z.shape[1]] = z
+        result[:z.shape[0], :z.shape[1]] = z
     else:
         while numrows > 50:
-            z = np.delete(z,-1,0)
+            z = np.delete(z, -1, 0)
             numrows = len(z)
         result = z
     return result
 
 #definition
-data_list1=[]
+data_list1 = []
 DATA_DIR1 = './Train_Data/'
 listing1 = os.listdir(DATA_DIR1)
-num_samples1=len(listing1)
+num_samples1 = len(listing1)
 
 #creates 3D array
 for filename in os.listdir(DATA_DIR1):
-    x = np.genfromtxt(DATA_DIR1 + filename,delimiter=',')    
+    x = np.genfromtxt(DATA_DIR1 + filename, delimiter=',')
     normalized = reshape(x)
     reshaped = normalized.reshape(1, 50, 37)
     data_list1.append(reshaped)
@@ -42,21 +39,9 @@ data_array1 = np.vstack(data_list1)
 
 
 #one hot encodes the data set
-#TODO create a more efficient method for this
-label1=np.ones((num_samples1,13),dtype = int)
-label1[0:200] = [1,0,0,0,0,0,0,0,0,0,0,0,0]
-label1[200:400] = [0,1,0,0,0,0,0,0,0,0,0,0,0]
-label1[400:600] = [0,0,1,0,0,0,0,0,0,0,0,0,0]
-label1[600:800] = [0,0,0,1,0,0,0,0,0,0,0,0,0]
-label1[800:1000] = [0,0,0,0,1,0,0,0,0,0,0,0,0]
-label1[1000:1200] = [0,0,0,0,0,1,0,0,0,0,0,0,0]
-label1[1200:1400] = [0,0,0,0,0,0,1,0,0,0,0,0,0]
-label1[1400:1600] = [0,0,0,0,0,0,0,1,0,0,0,0,0]
-label1[1600:1800] = [0,0,0,0,0,0,0,0,1,0,0,0,0]
-label1[1800:2000] = [0,0,0,0,0,0,0,0,0,1,0,0,0]
-label1[2000:2200] = [0,0,0,0,0,0,0,0,0,0,1,0,0]
-label1[2200:2400] = [0,0,0,0,0,0,0,0,0,0,0,1,0]
-label1[2400:] = [0,0,0,0,0,0,0,0,0,0,0,0,1]
+label1 = np.ones((num_samples1, 13), dtype=int)
+for p in range(13):
+    label1[200*p:200*(p+1)] = [0]*p+[1]+[0]*(13-p)
 """label[488:522] = [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0]
 label[522:557] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
 label[557:596] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0]
@@ -73,19 +58,19 @@ label[959:] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]"""
 
 
 #indexs the arrays with the label
-train_data1=[data_array1,label1]
-(X,Y) = (train_data1[0],train_data1[1])
+train_data1 = [data_array1, label1]
+X, Y = train_data1[0], train_data1[1]
 
 #randomizes data set and splits into train and test sets
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, random_state=4)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=4)
 
 #creates model
 np.random.seed(7)
 model = Sequential()
-model.add(Dense(200, input_shape=(50,37), activation='tanh'))
-model.add(LSTM(200, dropout=0.2,recurrent_dropout=0.2))
+model.add(Dense(200, input_shape=(50, 37), activation='tanh'))
+model.add(LSTM(200, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(13, activation='softmax'))
-model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
 #iterates model over data set
