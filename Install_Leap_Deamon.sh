e9faa9c8c0d6d2572d8f4174180ded0d0c5b6936
Murray Fordyce <Murray.Fordyce@gmail.com>
2018-11-27 07:06:22 -0600

    Don't ask user to confirm LEAP installation

--- Install_Leap_Deamon.sh
+++ Install_Leap_Deamon.sh
@@ -3,8 +3,8 @@
 # set -e
 # todo: finalize this
 
-dpkg --install LEAP/Leap-2.3.1+31549-x64.deb || echo "Ignore fail due to unmaintained package"
-apt-get install -f
+dpkg --install -y LEAP/Leap-2.3.1+31549-x64.deb || echo "Ignore fail due to unmaintained package"
+apt-get install -f -y
 cp LEAP/leapd.service /lib/systemd/system/leapd.service
 # you might need to ln -s /lib/systemd/system/leapd.service /etc/systemd/system/leapd.service
 systemctl enable leapd
